01247bf104264b44eec9f7f63ea005789dbba2de
Jonathan <jonzheng137@gmail.com>
2024-03-18 16:00:47 -0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jonathan Zheng on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
